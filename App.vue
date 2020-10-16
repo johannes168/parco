2ffbfc26c6cd7bcc222aa67bc778760f20d57db1
johannes168 <60751172+johannes168@users.noreply.github.com>
2020-10-16 16:04:39 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Parco logo" src="./assets/comingsoon.jpg">
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+body{
+  background-color: black;
+}
+
+img {
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   width: 417px;
+   height: 584px;
+   margin-top: -292px; /* Half the height */
+   margin-left: -208px; /* Half the width */
+}
+
+</style>
